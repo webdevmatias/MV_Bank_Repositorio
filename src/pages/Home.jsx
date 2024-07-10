10049f53e156489b5e49ab0d42190cdc271efd9d
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-07-10 20:54:20 -0300

    Atualização da home geral

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
+import piggy from '/piggy.jpg';
+
 const Home = () => {
     return (
         <section className="w-full h-screen grid md:grid-cols-2">
-            <div className="bg-yellow-500 md:col-span-1 h-full
-            flex flex-col justify-center items-center">
-                Item 1
+            <div className="bg-white md:col-span-1 w-full h-full flex flex-col justify-center items-stretch pl-16 pt-32 md:pt-0">
+                <h1 className="text-4xl font-bold mb-4">Olá! Conheça o MVBank!</h1>
+                <p className="text-2xl">O MVBank é a plataforma mais confiável e segura do mundo para você guardar, movimentar e investir seu dinheiro.</p>
             </div>
-            <div className="bg-green-500 md:col-span-1 h-full
-            flex flex-col justify-center items-center">
-                Item 2
+            <div className="bg-white md:col-span-1 h-full flex flex-col justify-center items-center">
+                <img src={piggy} className='h-1/2' alt="Piggy Bank" />
             </div>
         </section>
     );
 };
 
 export default Home;
+
+
