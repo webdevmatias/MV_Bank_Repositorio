efb1ac1418b1348c964565855b192af200cdc790
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-07-10 19:56:10 -0300

    atualizacao do header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
+import { BsCoin } from "react-icons/bs";
+
 
 const Header = () => {
   return (
-    <nav className="w-full h-auto bg-red-500 flex flex-row justify-around p-4
+    <nav className="w-full h-auto bg-gray-800 flex flex-row justify-around p-4
     fixed top-0 z-10">
-      <Link to="/">
-          <h1>MVBank</h1>
+      <Link className='flex flex-rol justify-center items-center gap-2' to="/">
+        <BsCoin className='text-white w-6 h-6' />
+        <h1 className='text-white'>MVBank</h1>
       </Link>
       <div className="flex flex-row gap-4">
-        <Link to="/login">
-          <button>Login</button>
+        <Link to="/login" >
+          <button className='text-white'>Login</button>
         </Link>
         <Link to="/cadastro">
-          <button>Cadastre-se</button>
+          <button className='text-white'>Cadastre-se</button>
         </Link>
       </div>
     </nav>
