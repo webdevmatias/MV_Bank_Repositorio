33ebb3e6daf5848d43305851c2e276399085f2f7
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-07-10 21:32:08 -0300

    atualizacao footer e home

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,35 @@
+import { FaEnvelope, FaPhone, FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
 const Footer = () => {
-    return(
-        <>
-        <nav className="w-full h-auto bg-black p-4
-        flex flex-row justify-around">
-            <h1 className="text-white">FOOTER</h1>
-            <button className="text-white">Contate-nos</button>
-        </nav>
-        </>
+    return (
+        <footer className="w-full h-auto bg-black p-4 flex flex-col md:flex-row justify-around items-center text-center md:text-left">
+            <div className="flex flex-col items-center md:flex-row md:items-center md:space-x-4 mb-4 md:mb-0 w-full md:w-auto">
+                <h1 className="text-white mb-4 md:mb-0">Contate-nos:</h1>
+                <a href="mailto:contato@mvbank.com" className="text-white hover:text-gray-400 flex items-center space-x-2 mb-2 md:mb-0">
+                    <FaEnvelope size={24} />
+                    <span>teste@gmail.com</span>
+                </a>
+                <a href="tel:+123456789" className="text-white hover:text-gray-400 flex items-center space-x-2">
+                    <FaPhone size={24} />
+                    <span>+55 0000-0000</span>
+                </a>
+            </div>
+            <div className="flex flex-row space-x-4">
+                <a href="https://www.facebook.com" className="text-white hover:text-gray-400">
+                    <FaFacebook size={32} />
+                </a>
+                <a href="https://www.twitter.com" className="text-white hover:text-gray-400">
+                    <FaTwitter size={32} />
+                </a>
+                <a href="https://www.instagram.com" className="text-white hover:text-gray-400">
+                    <FaInstagram size={32} />
+                </a>
+                <a href="https://www.linkedin.com" className="text-white hover:text-gray-400">
+                    <FaLinkedin size={32} />
+                </a>
+            </div>
+        </footer>
     );
-}; export default Footer
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,16 @@ import piggy from '/piggy.jpg';
 
 const Home = () => {
     return (
-        <section className="w-full h-screen grid md:grid-cols-2">
-            <div className="bg-white md:col-span-1 w-full h-full flex flex-col justify-center items-stretch pl-16 pt-32 md:pt-0">
-                <h1 className="text-4xl font-bold mb-4">Olá! Conheça o MVBank!</h1>
-                <p className="text-2xl">O MVBank é a plataforma mais confiável e segura do mundo para você guardar, movimentar e investir seu dinheiro.</p>
+        <section className="w-full h-screen grid grid-cols-1 md:grid-cols-2">
+            <div className="bg-white col-span-1 w-full h-full flex flex-col justify-center items-stretch p-8 md:pl-16 pt-32 md:pt-0">
+                <h1 className="text-3xl md:text-4xl font-bold mb-4">Olá! Conheça o MVBank!</h1>
+                <p className="text-lg md:text-2xl">O MVBank é a plataforma mais confiável e segura do mundo para você guardar, movimentar e investir seu dinheiro.</p>
             </div>
-            <div className="bg-white md:col-span-1 h-full flex flex-col justify-center items-center">
-                <img src={piggy} className='h-1/2' alt="Piggy Bank" />
+            <div className="bg-white col-span-1 h-full flex flex-col justify-center items-center p-8 md:p-0">
+                <img src={piggy} className='w-3/4 md:w-1/2' alt="Piggy Bank" />
             </div>
         </section>
     );
 };
 
 export default Home;
-
-
